f07aa8792f95164b0e5a64e24d2e49fce1fd88b1
yirankai <mikasasugoi@gmail.com>
2026-04-30 13:30:11 +0000

    Changes
    Co-authored-by: yirankai <223437073+yirankai@users.noreply.github.com>

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,6 @@
 import { Outlet, Link, createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
+import { AuthProvider } from "@/lib/auth";
+import { Toaster } from "@/components/ui/sonner";
 
 import appCss from "../styles.css?url";
 
